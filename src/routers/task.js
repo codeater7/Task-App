0e81b6b04c600678482f2d3fcfb9bee671acb7a0
Sujan <pokharelsujan134@gmail.com>
2020-03-02 20:08:19 +0200

    why not being pushed

--- src/routers/task.js
+++ src/routers/task.js
@@ -68,7 +68,8 @@ router.get('/tasks/:id', async (req, res) => {
         const task = await Task.findById(_id);
         if (!task) {
 			return res.status(404).send();
-		}
+        }
+        
 		res.send(task);
 	} catch (error) {
 		res.status(500).send();
